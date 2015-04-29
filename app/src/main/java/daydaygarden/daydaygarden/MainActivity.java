923c2ccaa2db7a2b8e5b7b232ba5a275f398f85f
fangtingting12 <1119348287@qq.com>
2015-04-29 20:07:42 +0800

    第二次修改 20:07

--- app/src/main/java/daydaygarden/daydaygarden/MainActivity.java
+++ app/src/main/java/daydaygarden/daydaygarden/MainActivity.java
@@ -1,19 +1,73 @@
 package daydaygarden.daydaygarden;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements RadioGroup.OnCheckedChangeListener {
+
+    private RadioGroup mGroup;
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        if (!checkNetwork()) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("网络连接失败").setMessage("请检查网络设置").setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Intent intent = new Intent("android.settings.WIRELESS_SETTINGS");
+                    startActivity(intent);
+                }
+            }).setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    return;
+                }
+            }).create().show();
+        }
+        mGroup= (RadioGroup) findViewById(R.id.radiogroup);
+        mGroup.setOnCheckedChangeListener(this);
+        ((RadioButton)mGroup.getChildAt(0)).setChecked(true);
+        mToolBar= (Toolbar) findViewById(R.id.toolbar);
+
+    }
+    private boolean checkNetwork() {
+        ConnectivityManager conn = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net = conn.getActiveNetworkInfo();
+        if (net != null && net.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+    long currentTime;
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - currentTime > 2000) {
+            currentTime = System.currentTimeMillis();
+            Toast.makeText(MainActivity.this, "再按一次退出",
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            finish();
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +89,26 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCheckedChanged(RadioGroup group, int checkedId) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        Fragment fragment=null;
+        switch (checkedId){
+            case R.id.radio1:
+
+                break;
+            case R.id.radio2:
+                break;
+            case R.id.radio3:
+                break;
+            case R.id.radio4:
+                break;
+            case R.id.radio5:
+                break;
+
+        }
+        ft.replace(R.id.fragment_container,fragment).commit();
     }
 }
